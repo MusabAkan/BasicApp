3a25dc160fc2f426d0b7b4616b7fafd511632dd1
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2023-09-22 21:34:56 +0300

    #dont try me

--- BasicApp.Web/Controllers/CartController.cs
+++ BasicApp.Web/Controllers/CartController.cs
@@ -1,4 +1,6 @@
 ﻿using BasicApp.Bussiness.Abstract;
+using BasicApp.Entities.Concrete;
+using BasicApp.Web.Models;
 using BasicApp.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,13 +21,22 @@ namespace BasicApp.Web.Controllers
 
         public ActionResult AddToCart(int productId)
         {
-            
             var productToBeAdded = _productService.GetById(productId);
             var cart = _cartSessionService.GetCart();
             _cartService.AddToCart(cart, productToBeAdded);
             _cartSessionService.SetCart(cart);
             TempData.Add("message", $"Your product, {productToBeAdded.ProductName} was successfuly added to the cart!");
             return RedirectToAction("Index", "Product");
+        }
+
+        public ViewResult List()
+        {
+            Cart cart = _cartSessionService.GetCart();
+            CartListViewModel model = new ()
+            {
+                Cart = cart
+            };
+            return View(model);
         }
     }
 }
---
+++ BasicApp.Web/Models/CartListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using BasicApp.Entities.Concrete;
+
+namespace BasicApp.Web.Models
+{
+    public class CartListViewModel
+    {
+        public Cart Cart { get; set; }
+    }
+}
